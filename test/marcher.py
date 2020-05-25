2ec207185f525b0fa1276ae069fb96170dda46eb
lenixlobo <lenixlobo@gmail.com>
2020-05-25 11:39:35 +0530

    Update marcher.py

--- test/marcher.py
+++ test/marcher.py
@@ -134,6 +134,8 @@ mapper.AddShaderReplacement(
     	float light = dot(ld, norm);
     	
     	fragOutput0 = vec4( vec3(1.0, 1.0, 1.0) *  light , 1.0);
+    	//fragOutput0 = vec4( norm, 1.0);
+    	
     }
     else{
 
